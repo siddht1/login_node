ded0bcd201bebd55570de28fc4ea33a736090412
siddht1 <rt1yrru+github@gmail.com>
2023-06-20 21:08:29 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,37 +1,33 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-
+const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Enable CORS for specific origin
 app.use(cors({
-//   origin: "https://example.com"
+  // origin: "https://example.com"
 }));
-app.use(express.static('static'));
-// Parse request body and extended the size to 1mb
 
+// Define the static files directory
+app.use(express.static(path.join(__dirname, "static")));
+
+// Parse request body and extended the size to 1mb
 app.use(bodyParser.json({ limit: '1mb' }));
 app.use(bodyParser.urlencoded({ limit: '1mb', extended: true }));
 
 // GET route
 app.get("/", (req, res) => {
-  let data = {};
-  data["GET"] = req.query;
-  res.sendFile(__dirname + '/static/index.html');
-  // res.sendFile(__dirname+'index.html');
-  //      let filePath = path.join(__dirname);
-  // res.sendFile(filePath);
-// res.send(__dirname);
+  res.sendFile(path.join(__dirname, "static", "index.html"));
 });
 
 // POST route
 app.post("/", (req, res) => {
   console.log("POST request received");
-  let data={};
-   data['POST'] = req.body;
+  let data = {};
+  data['POST'] = req.body;
   res.send(data);
 });
 
