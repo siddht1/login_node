01fc4d32523b12d2c9d39b41353ab13d4de8f48c
siddht1 <rt1yrru+github@gmail.com>
2023-06-20 13:46:20 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ app.get("/", (req, res) => {
   let data = {};
   data["GET"] = req.query;
   // res.sendFile(__dirname + '/static/index.html');
-  res.sendFile(__dirname+'static/index.html');
+  res.sendFile(__dirname+'index.html');
 });
 
 // POST route
