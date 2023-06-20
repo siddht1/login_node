80ab2f8dae45cacb37fa5205bd6d942fc3bbe28c
siddht1 <rt1yrru+github@gmail.com>
2023-06-20 20:57:09 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const path = require('path');
 const fs = require('fs');
 //joining path of directory 
-const directoryPath = path.join(__dirname, 'Documents');
+const directoryPath = path.join(__dirname);
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -26,8 +26,19 @@ app.get("/", (req, res) => {
   data["GET"] = req.query;
   // res.sendFile(__dirname + '/static/index.html');
   // res.sendFile(__dirname+'index.html');
-       let filePath = path.join(__dirname);
-  res.sendFile(filePath);
+  //      let filePath = path.join(__dirname);
+  // res.sendFile(filePath);
+  fs.readdir(directoryPath, function (err, files) {
+    //handling error
+    if (err) {
+        return console.log('Unable to scan directory: ' + err);
+    } 
+    //listing all files using forEach
+    files.forEach(function (file) {
+        // Do whatever you want to do with the file
+        console.log(file); 
+    });
+});
 });
 
 // POST route
