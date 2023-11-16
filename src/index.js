d14dbd2c3708a055e7e9b18cffaca1e1d7304ba6
siddht1 <rt1yrru+github@gmail.com>
2023-11-16 13:55:21 +0530

    3

--- src/index.js
+++ src/index.js
@@ -1,40 +1,27 @@
-// In src/index.js
-import express from 'express';  
-import cors from 'cors';  
-import { ulid } from 'ulid';  
-import bodyParser from 'body-parser';  
-import helmet from 'helmet';  
-import morgan from 'morgan';  
-
+import express from 'express';
+import cors from 'cors';
+import { ulid } from 'ulid';
+import bodyParser from 'body-parser';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import path from 'path';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors());  
-app.use(bodyParser.json());  
-app.use(morgan('combined'));  
-app.use(helmet());  
-
-const ulidgen=ulid();
- 
+app.use(cors());
+app.use(bodyParser.json({ limit: '1mb' })); // Remove redundant bodyParser.urlencoded()
+app.use(morgan('combined'));
+app.use(helmet());
 
 app.use(express.static(path.join(__dirname, '../public')));
-app.use(bodyParser.json({ limit: '1mb' }));
-app.use(bodyParser.urlencoded({ limit: '1mb', extended: true }));  
-
-// root
-
-
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '../views'));
-const registerEjsPath = path.join(__dirname, "../views/register.ejs");
 
-app.all("*", (req, res) => {
-  res.render(registerEjsPath);
+app.all('/', (req, res) => { // Specify the correct route path
+  res.render('register.ejs'); // Use direct view name instead of variable
 });
-
-
 
 app.listen(PORT, () => {
   console.log(`API is listening on port ${PORT}`);
