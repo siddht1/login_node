43224daf2f5c6716a42bd1cfebc78bff945a0f6f
siddht1 <rt1yrru+github@gmail.com>
2023-11-16 13:24:29 +0530

    2

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 // In src/index.js
-const express = require("express");
+import express from 'express';  
+import cors from 'cors';  
+import { ulid } from 'ulid';  
+import bodyParser from 'body-parser';  
+import helmet from 'helmet';  
+import morgan from 'morgan';  
 
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// *** REMOVE ***
-app.get("/", (req, res) => {
+app.use(cors());  
+app.use(bodyParser.json());  
+app.use(morgan('combined'));  
+app.use(helmet());  
+
+const ulidgen=ulid();
+  
+
+// root
+
+app.all("*", (req, res) => {
   res.send("<h2>It's Working!</h2>");
 });
 
