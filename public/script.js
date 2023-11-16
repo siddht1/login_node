8147429f8f972f6d2feccf42f8741a88a6e7c819
siddht1 <rt1yrru+github@gmail.com>
2023-11-16 18:26:39 +0530

    8

--- public/script.js
+++ public/script.js
@@ -105,7 +105,8 @@ function done() {
   
   // add the h1 at the end with the welcome text  
   var h1 = document.createElement('h1');  
-  h1.appendChild(document.createTextNode('Welcome ' + questions[0].value + '!' +'to KIVTECHS.cloud'));  
+  h1.appendChild(document.createTextNode('Welcome ' + questions[0].value + '' +'to KIVTECHS.cloud'));  
+    h1.appendChild(document.createTextNode('Your Account is being created at the moment '));  
   setTimeout(function() {  
     register.parentElement.appendChild(h1);  
     setTimeout(function() {h1.style.opacity = 1}, 50);  
